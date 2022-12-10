080a1842f86b8bb2e9bafe728f2c75d4aa284c09
Muffcata <marta.oched@gmail.com>
2022-12-10 17:49:34 +0100

    task 9, 10 in progress

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const colorBackgr = document.querySelector("body");
+const btn = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
+const selectColor = () => {
+  const chooseColor = getRandomHexColor();
+  span.textContent = chooseColor;
+  btn.style.background = chooseColor;
+  colorBackgr.style.background = chooseColor;
+};
+
+btn.addEventListener("click", selectColor);
